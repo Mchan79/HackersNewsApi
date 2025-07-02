e64948db466318397ee72b65e453092608a1039e
Marco Antonio Chan Cruz <marcoantoniochan@hotmail.com>
2025-07-02 14:02:26 -0600

    Add console app to test api and rate the limit of calls

---
+++ TopStoriesConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TopStoriesConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<Start console app to test TopStoriesApi>");
+
+            //set the numbers of stories to fetch
+            var _numberOfStories = 2;
+            //set the maximum number of threads
+            var _max = 3;
+            //set the time in seconds to wait until the max number {max} of threads are finished  
+            var _time = 3;
+            
+            //create a new instance of ThreadSyncTest
+            var sync = new ThreadSyncTest(max: _max, time: _time, stories: _numberOfStories);
+            
+            sync.Start(10);
+
+            Console.WriteLine("<End>");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TopStoriesConsoleApp/ThreadSyncTest.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopStoriesApi.Models;
+
+namespace TopStoriesConsoleApp
+{
+    internal class ThreadSyncTest
+    {
+        private readonly int MAX;
+        private readonly int TIME;
+        private readonly int _stories;
+
+        private List<Item> _calls;
+        private object syncCall = new object();
+        private AutoResetEvent _areCalls = new AutoResetEvent(true);
+        private AutoResetEvent _areSync = new AutoResetEvent(true);
+        private int _index;
+
+        public ThreadSyncTest(int max, int time, int stories)
+        {
+            MAX = max;
+            TIME = time;
+            _calls = new List<Item>();
+            _stories = stories;
+        }
+
+        public void Start(int count)
+        {
+            var items = Enumerable.Range(1, count);
+
+            var tasks = new List<Task>();
+
+            items.ToList().ForEach(item =>
+            {
+                var task = this.Running(item);
+                tasks.Add(task);
+            });
+
+            Task.WaitAll(tasks.ToArray());
+        }
+        private Task Running(int index)
+        {
+            return Task.Run(() =>
+            {
+                var isReady = false;
+                do
+                {
+                    _areCalls.WaitOne();
+                    lock (syncCall)
+                    {
+                        if (_calls.Count > 0)
+                        {
+                            var first = _calls[0];
+                            var dif = DateTime.Now - first.Date;
+                            if (dif.TotalSeconds > TIME)
+                            {
+                                Console.WriteLine($"Call {index} - Time exceeded: {dif.TotalSeconds} seconds");
+                                _calls.RemoveAt(0);
+                            }
+                        }
+                        if (_calls.Count < MAX)
+                        {
+                            isReady = true;
+                        }
+                        if (!isReady)
+                        {
+                            _areCalls.Set();
+                        }
+                    }
+                }
+                while (!isReady);
+
+                _areSync.WaitOne();
+                lock (syncCall)
+                {
+                    _calls.Add(new Item
+                    {
+                        Index = index,
+                        Date = DateTime.Now
+                    });
+                }
+                this.RunningHelper(index);
+                _areSync.Set();
+                _areCalls.Set();
+
+            });
+        }
+        private void RunningHelper(int index)
+        {
+            _index++;
+            Console.WriteLine($"{_index} - Running: {index} <START>");
+            Thread.Sleep(TimeSpan.FromSeconds(.2));
+
+            var client = new HttpClient();
+            var baseUrl = "http://localhost:5256";
+
+            var url = $"{baseUrl}/api/stories/best?n={_stories}";
+
+            var response = client.GetAsync(url).GetAwaiter().GetResult().Content.ReadAsStreamAsync();
+            var result = new StreamReader(response.Result).ReadToEnd();
+            var json = JsonConvert.DeserializeObject<List<StoryDto?>>(result);
+
+            Console.WriteLine($"Call {index}");
+            if (json != null)
+            {
+                Console.WriteLine($"Call {index} - Stories: {json.Count}");
+
+                foreach (var story in json)
+                {
+                    if (story != null)
+                    {
+                        Console.WriteLine($"Call {index} - Story: {story.CommentCount} - {story.Score} - Story: {story.Title} - {story.Uri} - {story.PostedBy}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Call {index} - Story: NULL");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Call {index} - No stories found");
+            }
+
+            Console.WriteLine($"{_index} - Running: {index} <END>\n");
+
+
+        }
+
+
+    }
+
+    internal class Item
+    {
+        public int Index { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
